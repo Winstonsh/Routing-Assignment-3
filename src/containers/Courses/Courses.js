70dfce9f8dfd2f3e1c2ee490ffba665a588a132c
Winstonsh <winston_halim@hotmail.com>
2020-09-03 11:18:43 +1000

    added nested route with parameter and output course ID

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+import { Link, Route } from 'react-router-dom';
+
 import './Courses.css';
+import Course from '../Course/Course';
 
 class Courses extends Component {
     state = {
@@ -18,10 +21,15 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return (
+                                <Link key={course.id} to={this.props.match.url + '/course/' + course.id}>
+                                    <article className="Course">{course.title}</article>
+                                </Link>
+                            );
                         } )
                     }
                 </section>
+                <Route path={this.props.match.url + '/course/:id'} component={Course}/>
             </div>
         );
     }
